b9c4f05fe4946e05558210f93006ff6458f3febc
Ashley M. Moore <mooream2291@gmail.com>
2021-09-28 23:00:25 -0400

    nav bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import { MenuItems } from './MenuItems';
+import './Navbar.css';
+import { Buttons } from '../Buttons';
 
 class Navbar extends Component {
+  state = { clicked: false }
+
+  clickHandler = () => {
+    this.setState({ clicked: !this.state.clicked })
+  }
+
+  //write a function to tell us if the button was licked or not
   render() {
     return(
-      <nav className="NavbarItem">
-        <h1 className="navbar-logo">React</h1>
-        <div className="menu-icon">
-
+      <nav className="NavbarItems">
+        <h1 className="navbar-logo">React<i className="fab fa-react"></i></h1>
+        <div className="menu-icon" onClick={this.clickHandler}>
+          <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
         </div>
-        <ul>
-          <li><a href=""></a></li>
+        <ul className={this.state.clicked ? 'mav-menu active' : 'nav-menu'}>
+          {MenuItems.map((item, index) => {
+              return (
+                <li key={index}>
+                  <a className={item.cName} href={item.url}>
+                  {item.title}
+                  </a>
+                </li>
+              )
+          })}
         </ul>
       </nav>
     )
