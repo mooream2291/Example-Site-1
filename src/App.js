8eddb4fd4beb3035a1c1ab0122e40166f7678edb
Ashley M. Moore <mooream2291@gmail.com>
2021-10-08 14:28:33 -0400

    redbuiling nav with styled components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
 import TestStyledComp from './components/TestComp';
-import { Grid, Row, Column } from './components/Grid';
-// import NavIcons from './components/Icons/IconNav';
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
+import Navbar from './components/Navbar/Navbar';
+
 function App() {
   return (
     <div className='App'>
-      <h1>Ashley Moore</h1>
-      <h3>Software Engineer</h3>
-      <Grid>
-        <Row>
-          <Column size={3}>
-          </Column>
-        </Row>
-        <Row>
-          <Column size={2}>
-            I WILL EXPAND
-          </Column>
-          <Column size={1} collapse="xs">
-            byeeeee
-          </Column>
-          <Column size={1}>
-            I WILL EXPAND
-          </Column>
-        </Row>
-      </Grid>
+      <Navbar />
       <TestStyledComp />
     </div>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,38 +1,14 @@
-import React, { Component } from 'react';
-import { MenuItems } from './MenuItems';
-import './Navbar.css';
-// import { Link } from 'react-router-dom';
+import React from 'react'
+import { Nav } from './Navbar.elements'
 
-class Navbar extends Component {
-  state = { clicked: false }
-
-  clickHandler = () => {
-    this.setState({ clicked: !this.state.clicked })
-  }
-
-  //write a function to tell us if the button was licked or not
-  render() {
-    return(
-      <nav className="NavbarItems">
-        {/* <Link to=""/> */}
-        <div className="menu-icon" onClick={this.clickHandler}>
-          <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-        </div>
-        <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-          {MenuItems.map((item, index) => {
-              return (
-                <li key={index}>
-                  <a className={item.cName} href={item.url}>
-                  {item.title}
-                  </a>
-                </li>
-              )
-          })}
-        </ul>
-      </nav>
-    )
-  }
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        I am Here.
+      </Nav>
+    </>
+  )
 }
-
 
 export default Navbar;
