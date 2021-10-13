719dcd49851e7906534ce04b03d157553b4a9eef
Ashley M. Moore <mooream2291@gmail.com>
2021-10-13 06:04:40 -0400

    added global button style

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -23,4 +23,25 @@ padding-left: 50px;
 }
 `;
 
+export const Button = styled.button`
+border-radius: 4px;
+background: ${({primary}) => (primary ? 'yellow' : 'blue') };
+white-space: nowrap;
+padding: ${({big}) => (big ? '12px 64px' : '10px 20px')};
+color: white;
+font-size: ${({fontBig}) => (fontBig ? '20px' : '16px')};
+outline: none;
+border: none;
+cursor: pointer;
+
+&:hover {
+  transition: all 0.3s ease-out;
+  background: white;
+  background: ${({primary}) => (primary ? 'blue' : 'yellow') };
+}
+
+@media screen and (max-width: 960px) {
+  width: 100%auto;
+}
+`
 export default GlobalStyle
